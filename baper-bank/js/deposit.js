c8d5c174cf58cef3bbbcc5d109687768417738c8
RZ-Milon <rzmilon@gmail.com>
2022-08-16 02:16:03 +0600

    add blance amount

---
+++ baper-bank/js/deposit.js
@@ -0,0 +1,31 @@
+// step-1: add event listner to the deposit button 
+document.getElementById('btn-deposit').addEventListener('click', function(){
+    // step-2: get the deposit amount from the deposit amount field
+    const depositField = document.getElementById('deposit-field');
+    const newDepositAmountString = depositField.value;
+    const newDepositAmount = parseFloat(newDepositAmountString);
+
+    // step-3: get the current deosit total
+    const depositTotalElement = document.getElementById('deposit-total');
+    const previousDepositTotalString = depositTotalElement.innerText;
+    const previousDepositTotal = parseFloat(previousDepositTotalString);
+    
+    // step-4: add numbers to set the total deposit
+    const currentDepositTotal = previousDepositTotal + newDepositAmount;
+
+    depositTotalElement.innerText = currentDepositTotal;
+
+    // step-5: get blance current total
+    const blanceTotal = document.getElementById('blance-total');
+    const previousBlanceTotalString = blanceTotalElement.innerText;
+    const previousBlanceTotal = parseFloat(previousBlanceTotalString);
+
+    // calculate current total balance
+    const currentBalanceTotal = previousBlanceTotal + newDepositAmount;
+
+    blanceTotalElement.innerText = currentBalanceTotal;
+
+    // step-7: clear the deposit field
+    depositField.value = '';
+})
+
